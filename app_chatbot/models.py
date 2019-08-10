21df489acb1dab26baf844f5e35f2c05c21100fd
Tomás Barcellos <tomasbarcellos@gmail.com>
2019-08-10 01:11:48 -0300

    Separa historia de suas partes

--- app_chatbot/models.py
+++ app_chatbot/models.py
@@ -18,18 +18,23 @@ class AcaoIntencao(models.Model):
 
 class Historia(models.Model):
     nome = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nome
+
+class PartesHistoria(models.Model):
+    historia = models.ForeignKey(Historia, on_delete = models.PROTECT)
     ordem = models.PositiveSmallIntegerField("ordem do elemento na história")
     componente = models.ForeignKey(AcaoIntencao, on_delete = models.PROTECT)
 
     def __str__(self):
         return self.nome
 
-
 class Texto(models.Model):
     id_acao_intencao = models.ForeignKey(AcaoIntencao, on_delete = models.PROTECT)
     texto = models.CharField(max_length=500)
 
     def __str__(self):
-        return self.nome
+        return self.texto
 
 
