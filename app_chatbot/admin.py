36f2bbdbf644316e57bf644d457e87cd2464d2d4
Tomás Barcellos <tomasbarcellos@gmail.com>
2019-08-10 01:13:13 -0300

    registra parte no admin

--- app_chatbot/admin.py
+++ app_chatbot/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import AcaoIntencao, Historia, Texto
+from .models import AcaoIntencao, Historia, PartesHistoria, Texto
 
 admin.site.register(AcaoIntencao)
 admin.site.register(Historia)
+admin.site.register(PartesHistoria)
 admin.site.register(Texto)
