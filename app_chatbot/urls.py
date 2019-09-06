d7ba39adf285ff0233ddb6c041943c97e6f49bf5
Tomás Barcellos <tomasbarcellos@gmail.com>
2019-09-05 21:39:43 -0300

    resolve tela de cadastro de textos

--- app_chatbot/urls.py
+++ app_chatbot/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.historia, name='historia'),
     path('historia', views.criar_historia, name='criar_historia'),
-    re_path(r'^criar/(?P<tipo>(acao|intencao))$', views.criar_acao_intencao, name='criar_teste'),
+    re_path(r'^criar/(?P<tipo>(acao|intencao))$', views.criar_acao_intencao, name='criar_acao_intencao'),
     re_path(r'^listar/(?P<tipo>(acao|intencao))$', views.listar_acoesintencoes, name='listar_acoes'),
 ]
